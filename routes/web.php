666785e938976cee432193fe2b033fa293d9b5c6
Katy Morris <katymorris@Katys-MBP.home>
2016-11-02 09:26:05 -0700

    added some test routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,32 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo 'Hello Laravel';
+});
+
+Route::get('hello/{name}', function($name) {
+	echo 'Hello ' . $name;
+});
+
+//Create an item
+Route::post('test', function() {
+	echo 'We just created an item';
+});
+
+//Read an item
+Route::get('test', function() {
+	echo '<form action="test" method="POST">';
+	echo '<input type="submit">';
+	echo '<input type="hidden" value="' . csrf_token() . '" name="_token">';
+	echo '</form>';
+});
+
+//update an item
+Route::put('test', function() {
+
+});
+
+//delete an item
+Route::delete('test', function() {
+
 });
